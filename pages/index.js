a9053e5a83a39aed7b12ce89cb6765239c16df26
Amir <amir.batyrggaliev@gmail.com>
2023-11-21 19:16:11 +0600

    Create login.js file

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import { useRouter } from "next/router";
+
 export default function Home() {
+    const router = useRouter();
+    const handleLogin = () => {
+        router.push("/login");
+    };
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-10">
+        <div className="flex flex-col items-center justify-center bg-slate-900	 min-h-screen py-2 gap-10">
             <h1 className="text-4xl font-bold">Welcome to Flag Guess Game!</h1>
             <div className="flex flex-col gap-4">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-7 rounded">
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-7 rounded"
+                    onClick={handleLogin}
+                >
                     Login
                 </button>
                 <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-7 rounded">
