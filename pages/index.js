821c5d1d385de4044ef9fac6f7c51f39186c6358
Amir <amir.batyrggaliev@gmail.com>
2023-11-21 19:01:38 +0600

    Adjust slightly welcome page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl font-bold mb-5">
-                Welcome to Flag Guess Game!
-            </h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-10">
+            <h1 className="text-4xl font-bold">Welcome to Flag Guess Game!</h1>
             <div className="flex flex-col gap-4">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-7 rounded">
                     Login
                 </button>
-                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-7 rounded">
                     Sign Up
                 </button>
             </div>
