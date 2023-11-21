e4c3337a23e64972cfd7769f83f0fa948f413917
Amir <amir.batyrggaliev@gmail.com>
2023-11-21 19:34:11 +0600

    Create signup.js file

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,11 @@ export default function Home() {
     const handleLogin = () => {
         router.push("/login");
     };
+    const handleSignup = () => {
+        router.push("/signup");
+    };
     return (
-        <div className="flex flex-col items-center justify-center bg-slate-900	 min-h-screen py-2 gap-10">
+        <div className="flex flex-col items-center justify-center text-center bg-slate-900 min-h-screen py-2 gap-10">
             <h1 className="text-4xl font-bold">Welcome to Flag Guess Game!</h1>
             <div className="flex flex-col gap-4">
                 <button
@@ -15,7 +18,10 @@ export default function Home() {
                 >
                     Login
                 </button>
-                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-7 rounded">
+                <button
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-7 rounded"
+                    onClick={handleSignup}
+                >
                     Sign Up
                 </button>
             </div>
